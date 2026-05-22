1739de4e1953d8ff9568508062621798b0366f58
patrickvinicusfs <petrickviniciusclima@gmail.com>
2026-05-22 19:17:03 -0300

    Adiciinando codigo na classe autor, AutorRepository e autorRequesteDTO.

--- biblioteca/src/main/java/br/com/escola/biblioteca/repository/AutorRepository.java
+++ biblioteca/src/main/java/br/com/escola/biblioteca/repository/AutorRepository.java
@@ -1,5 +1,10 @@
 package br.com.escola.biblioteca.repository;
 
-public class AutorRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.com.escola.biblioteca.entity.Autor;
+
+@Repository
+public interface AutorRepository extends JpaRepository<Autor, Long> {
 
 }
