9b0a41d587524159dae8f1dfb7c620c808663a39
Rayla Martins Ferreira <raylamartinsferrreira@gmail.com>
2026-05-22 18:09:02 -0300

    add: livroRepository

--- biblioteca/src/main/java/br/com/escola/biblioteca/repository/LivroRepository.java
+++ biblioteca/src/main/java/br/com/escola/biblioteca/repository/LivroRepository.java
@@ -1,5 +1,9 @@
 package br.com.escola.biblioteca.repository;
 
-public class LivroRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro,Long> {
 
 }
