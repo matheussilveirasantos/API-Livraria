ed6d23347acee38dc356811409246632b69644ae
Rayla Martins Ferreira <raylamartinsferrreira@gmail.com>
2026-05-23 08:41:05 -0300

    mod: nas classes que tem Livro

--- biblioteca/src/main/java/br/com/escola/biblioteca/dto/LivroRequestDTO.java
+++ biblioteca/src/main/java/br/com/escola/biblioteca/dto/LivroRequestDTO.java
@@ -1,4 +1,18 @@
 package br.com.escola.biblioteca.dto;
 
-public record LivroRequestDTO(String titulo, String isbn, Integer anoPublicacao, String genero, Long autorId) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record LivroRequestDTO(
+		@NotBlank(message="Nenhum atributo pode está vazio")
+		@NotNull(message="O titulo é obrigatória")
+		String titulo,
+		@NotNull(message="O isbn é obrigatório")
+		String isbn,
+		@NotNull(message="O ano de publicação é obrigatório")
+		Integer anoPublicacao,
+		@NotNull(message="O gênero é obrigatótio")
+		String genero,
+		@NotNull(message="O autor é obrigatório")
+		Long autorId) {
 }
--- biblioteca/src/main/java/br/com/escola/biblioteca/repository/LivroRepository.java
+++ biblioteca/src/main/java/br/com/escola/biblioteca/repository/LivroRepository.java
@@ -2,8 +2,9 @@ package br.com.escola.biblioteca.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import br.com.escola.biblioteca.entity.Livro;
 
 @Repository
 public interface LivroRepository extends JpaRepository<Livro,Long> {
-
+	
 }
