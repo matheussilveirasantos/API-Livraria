849aa5d0a55113343a708b140af05b281dc42fd6
SimoneBromer <simonebromer@gmail.com>
2026-05-26 18:06:35 -0300

    Corrige repository de Editora

--- biblioteca/src/main/java/br/com/escola/biblioteca/repository/EditoraRepository.java
+++ biblioteca/src/main/java/br/com/escola/biblioteca/repository/EditoraRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EditoraRepository extends JpaRepository<Editora, Lon {
+public interface EditoraRepository extends JpaRepository<Editora, Long> {
 
-    boolean existsByCnpj(String nome);
+    boolean existsByCnpj(String cnpj);
 
 }
